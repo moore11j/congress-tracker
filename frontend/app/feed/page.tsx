b9ea8ef5f0aac28c6ce3d41660939dccf49005b6
moore11j <moore11j@gmail.com>
2026-01-25 20:57:37 -0800

    Revert "Fix App Router props to await Promise params/searchParams"

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -1,10 +1,1 @@
-import Page from "../page";
-
-type Props = {
-  searchParams?: Promise<Record<string, string | string[] | undefined>>;
-};
-
-export default async function FeedPage({ searchParams }: Props) {
-  const sp = (await searchParams) ?? {};
-  return <Page searchParams={sp} />;
-}
+export { default } from "../page";
--- frontend/types/page-props.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  type PageProps = {
-    params?: Promise<Record<string, string | string[]>>;
-    searchParams?: Promise<Record<string, string | string[] | undefined>>;
-  };
-}
-
-export {};
